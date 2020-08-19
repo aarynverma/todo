3e8f5333b5a789e3f9aa98066174029a4ed9bddb
aarynverma <18100BTCSE02667@svvvindore.onmicrosoft.com>
2020-08-20 02:15:48 +0530

    Updates

---
+++ precache-manifest.9e5efcfb40619948da2d44b1216d22c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba490fbe5aa2fb53206bc64974b4e79a",
+    "url": "/http//:aarynverma.github.io/todolistapp/index.html"
+  },
+  {
+    "revision": "f4cf011bd004c5d95c3a",
+    "url": "/http//:aarynverma.github.io/todolistapp/static/css/main.148c063b.chunk.css"
+  },
+  {
+    "revision": "e5fb0446b06061693233",
+    "url": "/http//:aarynverma.github.io/todolistapp/static/js/2.e8adcde5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http//:aarynverma.github.io/todolistapp/static/js/2.e8adcde5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4cf011bd004c5d95c3a",
+    "url": "/http//:aarynverma.github.io/todolistapp/static/js/main.928efb9d.chunk.js"
+  },
+  {
+    "revision": "373a84725542e84441a7",
+    "url": "/http//:aarynverma.github.io/todolistapp/static/js/runtime-main.6bc85bde.js"
+  }
+]);
